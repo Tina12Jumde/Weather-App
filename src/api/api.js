62ebd21914dbc9c2247fc0e30ac519b4b6d0b690
Tina12Jumde <158752623+Tina12Jumde@users.noreply.github.com>
2024-07-06 14:46:41 +0530

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '25dfef73fa494448c52cce1aa4c63d58';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
